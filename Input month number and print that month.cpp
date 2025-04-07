9cacc1070a5692491651b2a94298bf4cf190ed46
Krishna Agrawal <krishnaagr405@gmail.com>
2025-04-07 20:36:39 +0530

    Create Input month number and print that month.cpp

---
+++ Input month number and print that month.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the number : ");
+    scanf("%d", &a);
+    if(a==1)
+    {
+        printf("Its Janurary\n");
+    }
+    else if(a==2)
+    {
+        printf("Its Feburary\n");
+    }
+    else if(a==3)
+    {
+        printf("Its March\n");
+    }
+    else if(a==4)
+    {
+        printf("Its April\n");
+    }
+    else if(a==5)
+    {
+        printf("Its May\n");
+    }
+    else if(a==6)
+    {
+        printf("Its June\n");
+    }
+    else if(a==7)
+    {
+        printf("Its July\n");
+    }
+    else if(a==8)
+    {
+        printf("Its Auguest\n");
+    }
+    else if(a==9)
+    {
+        printf("Its September\n");
+    }
+    else if(a==10)
+    {
+        printf("Its October\n");
+    }
+    else if(a==11)
+    {
+        printf("Its November\n");
+    }
+    else if(a==12)
+    {
+        printf("Its December\n");
+    }
+    else
+    {
+        printf("A year has only 12 months\n");
+    }
+}
